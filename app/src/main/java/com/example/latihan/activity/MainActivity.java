e8af377154a98392d517dc193a7abc568ce4e030
andy78kurniawan@gmail.com <lampard78>
2019-04-10 12:03:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.latihan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.latihan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
